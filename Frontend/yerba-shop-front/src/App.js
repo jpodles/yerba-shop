eebdd5a480757bf7099b7871088a7ebd1de92f6c
ScreamHG <hubertg489655@gmail.com>
2021-03-26 09:40:44 +0100

    Login

--- Frontend/yerba-shop-front/src/App.js
+++ Frontend/yerba-shop-front/src/App.js
@@ -4,8 +4,40 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import NotFound from "./components/NotFound";
 import ProductDetails from "./components/ProductDetails";
+import Login from "./components/Login";
+import './index.scss';
+import React, { useState } from 'react';
+import { findRenderedComponentWithType } from "react-dom/test-utils";
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const Log = details => {
+    console.log(details);
+
+    if(details.email == adminUser.email && details.password == adminUser.password){
+      console.log("logged in");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    }else{
+    console.log("Details do not match!");
+    setError("Details do not match!");
+   }
+  }
+  
+
+  const Logout = () => {
+    setUser({name:"",email: ""});
+  }
+
   return (
     <Router>
       <div className="App">
@@ -17,6 +49,18 @@ function App() {
             </Route>
             <Route path="/products/:id">
               <ProductDetails />
+            </Route>
+            <Route path="/login">
+              <div className="app">
+                {(user.email !="")? (
+                  <div className="welcome">
+                    <h2>Welcome, <span>{user.name}</span></h2>
+                    <button onClick={Logout}>Logout</button>
+                  </div>
+                ) :(
+                  <Login Log={Log} error={error}/>
+                )}
+              </div>
             </Route>
             <Route path="*">
               <NotFound />
