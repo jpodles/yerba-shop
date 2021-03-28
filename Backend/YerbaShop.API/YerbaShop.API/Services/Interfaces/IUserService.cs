85e2862fc7d0910cf7dc9904783ad145af8e52fe
Czodak <bartekch99@gmail.com>
2021-03-28 15:31:59 +0200

    Added user serivce, repository placeholder, model

---
+++ Backend/YerbaShop.API/YerbaShop.API/Services/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YerbaShop.API.Models;
+
+namespace YerbaShop.API.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public List<User> GetUsers();
+        public User GetUserById(string id);
+        public User DeleteUserById(string id);
+        public User CreateUser(User user);
+    }
+}
--- Backend/YerbaShop.API/YerbaShop.API/Startup.cs
+++ Backend/YerbaShop.API/YerbaShop.API/Startup.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using YerbaShop.API.Repositories.Implementations;
+using YerbaShop.API.Repositories.Interfaces;
+using YerbaShop.API.Services;
+using YerbaShop.API.Services.Interfaces;
 
 namespace YerbaShop.API
 {
@@ -19,7 +23,9 @@ namespace YerbaShop.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //  change on real repository when database will be created.
+            services.AddScoped<IUserRepository, UserRepositoryPlaceholder>();
+            services.AddScoped<IUserService, UserService>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
